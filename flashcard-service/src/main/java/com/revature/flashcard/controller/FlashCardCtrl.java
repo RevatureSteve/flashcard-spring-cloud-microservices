a5429f112ad3e209fe149c848ac12eae90c799ce
Steven Kelsey <steven.kelsey@revature.com>
2018-03-01 10:32:42 -0500

    added circuit breaking

--- flashcard-service/src/main/java/com/revature/flashcard/controller/FlashCardCtrl.java
+++ flashcard-service/src/main/java/com/revature/flashcard/controller/FlashCardCtrl.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.revature.flashcard.bean.FlashCard;
 
 @RestController
@@ -31,6 +32,7 @@ public class FlashCardCtrl {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@HystrixCommand(fallbackMethod="cachedFc")
 	@GetMapping("/getFc")
 	public FlashCard getFc(){
 		System.out.println("Request to FlashCardCtrl /getFc");
@@ -38,5 +40,9 @@ public class FlashCardCtrl {
 		FlashCard fc = restTemplate.getForObject(url, FlashCard.class);
 		System.out.println(fc + " from flashcard-service2");
 		return fc;
+	}
+	
+	public FlashCard cachedFc(){
+		return new FlashCard(1, "cached-q", "cached-a");
 	}
 }
